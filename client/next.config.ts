7144378be826faa7083b0c6889050531be7294ad
VadymPopov <boycristal10@gmail.com>
2025-04-10 23:40:34 -0400

    fix: add image domain to next.config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
+const imageDomains = process.env.NEXT_PUBLIC_IMAGE_DOMAINS
+  ? process.env.NEXT_PUBLIC_IMAGE_DOMAINS.split(",")
+  : ["localhost"];
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost"],
+    domains: imageDomains,
   },
 };
 
